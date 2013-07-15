933ee892a7b3aa0beb2ee1dd6f0ddd1c6d41aae4
hongjie <im.not.be@gmail.com>
2013-07-15 22:28:12 +0800

    test

--- src/test/SemHookTest.java
+++ src/test/SemHookTest.java
@@ -1,24 +1,27 @@
-package hook;
+package test;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.antlr.runtime.CommonToken;
 import org.antlr.runtime.tree.BaseTree;
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.CommandNeedRetryException;
 import org.apache.hadoop.hive.ql.Driver;
 import org.apache.hadoop.hive.ql.exec.FetchTask;
 import org.apache.hadoop.hive.ql.exec.FunctionRegistry;
 import org.apache.hadoop.hive.ql.exec.Task;
 import org.apache.hadoop.hive.ql.lib.Node;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.parse.ASTNode;
 import org.apache.hadoop.hive.ql.parse.AbstractSemanticAnalyzerHook;
 import org.apache.hadoop.hive.ql.parse.HiveParser;
 import org.apache.hadoop.hive.ql.parse.HiveSemanticAnalyzerHookContext;
 import org.apache.hadoop.hive.ql.parse.SemanticException;
 
-public class SemHook extends AbstractSemanticAnalyzerHook {
+public class SemHookTest extends AbstractSemanticAnalyzerHook {
 
  
 
@@ -26,43 +29,29 @@ public class SemHook extends AbstractSemanticAnalyzerHook {
 	public ASTNode preAnalyze(HiveSemanticAnalyzerHookContext context,
 			ASTNode ast) throws SemanticException {
 		// TODO Auto-generated method stub
-		
-	 // 防止下面的driver.run递归进入这个hook,先关闭这个hook
-		String hook = context.getConf().get("hive.semantic.analyzer.hook");
-		context.getConf().set("hive.semantic.analyzer.hook", "");
-		
-		
+		Field hivejarfield;
 		try {
-			String cmd = " create temporary function __py__  as 'python.Method'";
-			Driver driver = new Driver();
-			int responseCode = driver.run(cmd).getResponseCode();
-			if (responseCode != 0) {
-				driver.close();
-			}
-		} catch (CommandNeedRetryException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//深度遍历，修改ast的TOK_FUNCTION
-		try {
-			changeFunctionDSF(ast);
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			hivejarfield = HiveConf.class.getDeclaredField("hiveJar");
+			hivejarfield.setAccessible(true);
+			hivejarfield.set(context.getHive().getConf(),"iamnota.jar");
+			System.out.println("******" + context.getHive().getConf().getJar());
 		} catch (NoSuchFieldException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (HiveException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-	 //然后打开这个hook
-		context.getConf().set("hive.semantic.analyzer.hook", hook);
+	
 		
 		return super.preAnalyze(context, ast);
 	}
